b24e6bec096f507c4a5689546024c3089bc0891a
zacaway <mike.talbot@zacaway.com>
2013-03-21 18:59:00 -0600

    added prelude packages

--- personal/packages.el
+++ personal/packages.el
@@ -6,6 +6,12 @@
 
 ;;; Code:
 
+(require 'prelude-common-lisp)
+(require 'prelude-emacs-lisp)
+(require 'prelude-css)
+(require 'prelude-org)
+(require 'prelude-xml)
+
 (prelude-ensure-module-deps '(js2-mode js2-refactor))
 
 ;;; packages.el ends here

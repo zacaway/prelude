825f9b3924aff5b13580482e752e3e7b3c0113b1
zacaway <mike.talbot@zacaway.com>
2013-04-07 03:50:16 -0600

    added common-lisp.el

---
+++ personal/common-lisp.el
@@ -0,0 +1,6 @@
+;;; settings for CL dev
+
+;(setq cltl2-url "file:///usr/share/doc/cltl/clm/node1.html")
+
+(if (file-exists-p "/usr/share/doc/hyperspec")
+    (setq common-lisp-hyperspec-root "/usr/share/doc/hyperspec/"))

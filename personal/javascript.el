9b1e5e3e939d435b4b34605779cb8c66a84a0467
zacaway <mike.talbot@zacaway.com>
2013-06-11 15:27:18 -0600

    cleanup js config

--- personal/javascript.el
+++ personal/javascript.el
@@ -7,7 +7,6 @@
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 
 (autoload 'js2-mode "js2-mode" nil t)
-;;(autoload 'js2-highlight-vars-mode "js2-highlight-vars" nil t)
 
 ;; See: http://ternjs.net
 (add-to-list 'load-path "~/Projects/tern/emacs/")
@@ -16,17 +15,6 @@
 (add-hook 'js2-mode-hook
           (lambda () (if (locate-library "tern")
                          (tern-mode t))))
-
-;; (add-hook 'js2-mode-hook
-;;           (lambda ()
-;;             (add-to-list 'projectile-project-compilation-commands
-;;                          '("mvn compile clean" .
-;;                            (lambda (dir)
-;;                              (file-exists-p (expand-file-name "pom.xml" dir)))))
-;;             (add-to-list 'projectile-project-test-commands
-;;                          '("mvn clean install -Dtest=unit clean" .
-;;                            (lambda (dir)
-;;                              (file-exists-p (expand-file-name "pom.xml" dir)))))))
 
 (defun projectile-test-suffix (project-type)
   "Hard-coded test suffix for WBP project.  PROJECT-TYPE is ignored."

24238ac3cfc9cc791a10e71c5792bfa2e8af896a
zacaway <mike.talbot@zacaway.com>
2013-06-11 15:29:53 -0600

    reset fill-column to default

--- personal/custom.el
+++ personal/custom.el
@@ -3,7 +3,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(fill-column 100)
  '(js2-highlight-external-variables nil)
  '(projectile-svn-command "find . -name .svn -prune -o -type f -print0"))
 (custom-set-faces
